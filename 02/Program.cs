36d3501b645cd45fbaf24d4985bb46b296ff627b
Ali <net_lina95@mail.ru>
2023-07-09 16:46:45 +0300

    дз готово

---
+++ 02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.Write("Введите число: ");
+int r = Convert.ToInt32(Console.ReadLine());
+
+
+
+int ThirdDigit(int r)
+        {
+            
+            
+                if (r > 99)
+            
+                while (r > 999)
+                {
+                    r = r / 10;
+                }
+                int result = r % 10;
+            return result;
+                 
+
+        }
+
+if (r<100)
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine(ThirdDigit(r));
---
+++ 03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.Write("Введите день недели: ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+bool Nd(int d)
+{
+    if (d>7)
+        return true;
+    else return false;
+}
+
+bool B(int d)
+{
+    if (d<6 && d>0)
+        return true;
+    else return false;
+}   
+if (Nd(d))
+    Console.WriteLine("Такого дня недели не существует");
+else    
+    if (B(d))
+        Console.WriteLine("Не выходной:(");
+    else 
+        Console.WriteLine("ВЫХОДНОЙ!!!");
